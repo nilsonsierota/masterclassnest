29ee126a4e6ab34f43138932d3fd3734f78992ff
Nilson Sierota Correia <nilsoncv2@gmail.com>
2023-02-08 09:14:59 -0400

    feat: class-validation and class-transform

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { PrismaService } from './database/prisma.service';
+import { randomUUID } from 'node:crypto';
+import { CreateTeamMemberBody } from './dtos/create-team-member-body';
 
 @Controller('app')
 export class AppController {
   constructor(private prisma: PrismaService) {}
 
   @Get('hello')
-  async getHello() {
+  async getHello(@Body() body: CreateTeamMemberBody) {
+    const { name, function: MemeberFunction } = body;
+
     const member = await this.prisma.rocketTeamMember.create({
       data: {
-        id: 'id',
+        id: randomUUID(),
         name: 'name',
         function: 'function',
       },
