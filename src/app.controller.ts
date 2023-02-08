c29c25edb9c58105cf15c8df8a532fd2a5ac97e9
Nilson Sierota Correia <nilsoncv2@gmail.com>
2023-02-08 09:07:00 -0400

    feat: Prisma

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { PrismaService } from './database/prisma.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private prisma: PrismaService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('hello')
+  async getHello() {
+    const member = await this.prisma.rocketTeamMember.create({
+      data: {
+        id: 'id',
+        name: 'name',
+        function: 'function',
+      },
+    });
+
+    return { member };
   }
 }
